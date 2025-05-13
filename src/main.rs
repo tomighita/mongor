8f0dc3ca25c733852940be824c5e7c86bb0d9952
Victor Ghita <victorfilip.ghita@mongodb.com>
2025-05-13 15:25:54 +0100

    Catalog polling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer, rt::System, web};
 use mongodb::{Client, options::ClientOptions};
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
 
+mod catalog;
 mod config;
 mod routes;
 
 pub mod shared {
+    use std::sync::{Arc, Mutex};
+
     #[derive(Clone)]
     pub struct AppState {
         pub config: crate::config::AppConfig,
         pub db_client: mongodb::Client,
+        pub collections: Arc<Mutex<crate::catalog::Catalog>>,
     }
 }
 
@@ -18,18 +24,31 @@ async fn main() -> std::io::Result<()> {
 
     let options = ClientOptions::parse(&config.database_conn_url)
         .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-    let db_client = Client::with_options(options)
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        .expect("failed to parse config");
+    let db_client = Client::with_options(options).expect("failed to create client");
 
     println!("Successfully connected to MongoDB!");
 
+    let init_catalog = catalog::fetch_all_collections(&db_client.database(&config.database_name))
+        .await
+        .expect("Error fetching initial catalog");
+
+    // Create the shared state
+    let app_state = web::Data::new(crate::shared::AppState {
+        config: config.clone(),
+        db_client: db_client.clone(),
+        collections: Arc::new(Mutex::new(init_catalog)),
+    });
+
+    // Spawn a background task to periodically fetch catalog
+    actix_web::rt::spawn(catalog::fetch_collections_periodically(
+        app_state.clone(),
+        Duration::from_secs(5),
+    ));
+
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(crate::shared::AppState {
-                config: config.clone(),
-                db_client: db_client.clone(),
-            }))
+            .app_data(app_state.clone())
             .configure(routes::configure)
     })
     .bind(("127.0.0.1", 8080))?
